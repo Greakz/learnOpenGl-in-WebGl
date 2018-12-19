96e0aaaf0fbf73dbf73cb0d2cbf2fc5b46973951
l_thie13 <l_thie13@uni-muenster.de>
2018-12-19 11:48:57 +0100

    WIP: Lesson04-PhongAndSimpleMaterials

---
+++ Lesson04-PhongLightAndSimpleMaterials/Shader/Shader.glsl
@@ -0,0 +1,53 @@
+//#VERTEX-SHADER#//
+#version 300 es
+in vec3 VertexPosition;
+in vec3 VertexNormal;
+
+uniform mat4 model_matrix;
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
+
+out vec3 vSurfaceNormal;
+out vec3 vSurfacePosition;
+
+void main(void) {
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(VertexPosition, 1.0);
+    vSurfaceNormal = VertexNormal;
+    vSurfacePosition = vec3(model_matrix * vec4(VertexPosition, 1.0));
+}
+
+//#FRAGMENT-SHADER#//
+#version 300 es
+precision mediump float;
+in vec3 vSurfaceNormal;
+in vec3 vSurfacePosition;
+
+uniform vec3 mat_ambient;
+uniform vec3 mat_diffuse;
+uniform vec3 mat_specular;
+uniform float mat_shininess;
+
+uniform vec3 light_position;
+uniform vec3 camera_position;
+
+const vec3 light_color = vec3(1.0);
+
+out vec4 fragmentColor;
+
+void main(void) {
+
+    // ambient light
+    vec3 ambient_light_result = mat_ambient * light_color;
+    // diffuse light
+    vec3 surface_normal_unit = normalize(vSurfaceNormal);
+    vec3 light_dir = normalize(light_position - vSurfacePosition);
+    float diff_strength = max(dot(surface_normal_unit, light_dir), 0.0);
+    vec3 diff_light_result = vec3(diff_strength) * mat_diffuse * light_color;
+    // specular light
+    vec3 view_dir = normalize(camera_position - vSurfacePosition);
+    vec3 reflect_dir = reflect(-light_dir, surface_normal_unit);
+    float spec_strenght = pow(max(dot(view_dir, reflect_dir), 0.0), mat_shininess);
+    vec3 spec_light_result = mat_specular * vec3(spec_strenght) * light_color;
+
+    fragmentColor = vec4(ambient_light_result + diff_light_result + spec_light_result, 1.0);
+}
---
+++ Lesson04-PhongLightAndSimpleMaterials/src/Shader.ts
@@ -0,0 +1,45 @@
+import { ShaderLoader } from './BaseStack/ShaderLoader';
+import { Canvas } from './BaseStack/Canvas';
+
+export class Shader {
+
+    public program: WebGLProgram;
+
+    public attribute_locations: {
+        vertex_position: number;
+        vertex_normal: number;
+    };
+
+    public uniform_locations: {
+        model_matrix: WebGLUniformLocation;
+        view_matrix: WebGLUniformLocation;
+        projection_matrix: WebGLUniformLocation;
+        mat_ambient: WebGLUniformLocation;
+        mat_diffuse: WebGLUniformLocation;
+        mat_specular: WebGLUniformLocation;
+        mat_shininess: WebGLUniformLocation;
+        light_position: WebGLUniformLocation;
+        camera_position: WebGLUniformLocation;
+    };
+
+    create() {
+        this.program = ShaderLoader.buildShader('shader');
+        const GL: WebGL2RenderingContext = Canvas.getGL();
+        this.attribute_locations = {
+            vertex_position: GL.getAttribLocation(this.program, 'VertexPosition'),
+            vertex_normal: GL.getAttribLocation(this.program, 'VertexNormal'),
+        };
+        this.uniform_locations = {
+            model_matrix: GL.getUniformLocation(this.program, 'model_matrix'),
+            view_matrix: GL.getUniformLocation(this.program, 'view_matrix'),
+            projection_matrix: GL.getUniformLocation(this.program, 'projection_matrix'),
+            mat_ambient: GL.getUniformLocation(this.program, 'mat_ambient'),
+            mat_diffuse: GL.getUniformLocation(this.program, 'mat_diffuse'),
+            mat_specular: GL.getUniformLocation(this.program, 'mat_specular'),
+            mat_shininess: GL.getUniformLocation(this.program, 'mat_shininess'),
+            light_position: GL.getUniformLocation(this.program, 'light_position'),
+            camera_position: GL.getUniformLocation(this.program, 'camera_position')
+        };
+    }
+
+}
